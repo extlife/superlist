5d6e7e043e8e1a02f5d342f9383f6a92be2eff6d
extlife <extlife@yandex.ru>
2022-01-05 18:57:26 +0300

    The base view now returns minimal HTML

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest, response
 from lists.views import home_page
 
 # Create your tests here.
@@ -7,5 +8,15 @@ from lists.views import home_page
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page(self):
+        '''test: root url resolves to home page'''
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        '''test: home page returns correct html'''
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
