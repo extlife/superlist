883242396636e8f665c5f3e087dc893faaaafd98
extlife <extlife@yandex.ru>
2022-01-08 18:33:32 +0300

    Output to the page of the value entered by the user

--- lists/tests.py
+++ lists/tests.py
@@ -9,3 +9,9 @@ class HomePageTest(TestCase):
         '''test: home page returns correct html'''
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'lists/home.html')
+
+    def test_can_cave_a_POST_request(self):
+        '''test: can save post-request'''
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'lists/home.html')
