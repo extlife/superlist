1e48681ed7cef28fe4b922eb3517d5317c1c95cd
extlife <extlife@yandex.ru>
2022-01-09 14:41:46 +0300

    Redirect after POST and show all elements in the template

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+from django.http import response
 from django.test import TestCase
 from lists.models import Item
 
@@ -14,8 +15,30 @@ class HomePageTest(TestCase):
     def test_can_cave_a_POST_request(self):
         '''test: can save post-request'''
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'lists/home.html')
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        '''test: redirects after POST'''
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        '''test: only saves items when necessary'''
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        '''test: displays all list items'''
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('item 1', response.content.decode())
+        self.assertIn('item 2', response.content.decode())
 
 class ItemModelTest(TestCase):
     '''Item model test'''
