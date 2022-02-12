ab038887a6bc9d8b1f150c45a15278cd579f2c9e
extlife <extlife@yandex.ru>
2022-02-12 20:26:17 +0300

    FT first steps for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -113,5 +113,26 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
-        # # Удовлетворенные, они обе ложатся спать
+        # # Satisfied, they both go to bed
+
+    def test_layout_and_styling(self):
+        '''test: layout and styling'''
+        # Edith opens the homepage
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
         
+        # She notices that the input field is neatly centered
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512, delta=10)
+        
+        # She starts a new list and sees that the input field is 
+        # there too neatly centered
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512, delta=10)
